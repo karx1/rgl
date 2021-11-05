7e14e94178fad822d03a677ba3041a6f71af7bb5
Yash Karandikar <nerdstep710@gmail.com>
2021-11-05 13:24:44 -0500

    Add support for fetching from firebase

--- voters/src/main.rs
+++ voters/src/main.rs
@@ -1,10 +1,18 @@
 use sycamore::prelude::*;
+use wasm_bindgen_futures::spawn_local;
 
 fn main() {
+    let state = Signal::new(String::new());
+    spawn_local(cloned!((state) => async move {
+        let resp = surf::get(env!("FIREBASE_URL")).recv_string().await.unwrap();
+        let parsed = json::parse(&resp).unwrap();
+
+        state.set(parsed["votes"].to_string());
+    }));
     sycamore::render(|| {
         template! {
             div(class="wrapper") {
-                p { "Hello, world!" }
+                p { (state.get()) }
             }
         }
     });
