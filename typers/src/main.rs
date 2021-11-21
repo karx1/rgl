fc16cdecf9dc1276e23439bf2cfe8e45550ea156
Yash Karandikar <nerdstep710@gmail.com>
2021-11-21 13:35:49 -0600

    Validate typed chars

--- typers/src/main.rs
+++ typers/src/main.rs
@@ -1,24 +1,61 @@
 mod interval;
 
-use sycamore::prelude::*;
-use wasm_bindgen::closure::Closure;
 use crate::interval::set_interval;
+use sycamore::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_name = processText)]
+    fn process_text(value: String) -> usize;
+}
 
 fn main() {
     let time_left = Signal::new(60u8);
+    let value = Signal::new(String::new());
+    let error_count = Signal::new(0usize);
+    let current_quote = Signal::new(
+        String::from("The quick brown fox jumps over the lazy dog")
+            .chars()
+            .collect::<Vec<char>>(),
+    );
 
     let cb = Closure::wrap(Box::new(cloned!((time_left) => move || {
         let time = *time_left.get();
-        time_left.set(time - 1);
+        if time > 0 {
+            time_left.set(time - 1);
+        }
     })) as Box<dyn Fn()>);
 
     set_interval(&cb, 1_000);
 
-    cb.forget();
+    cb.forget(); // This leaks memory but without it the closure is dropped before it can be called by the interval
 
-    sycamore::render(|| template! {
-        div(class="wrapper") {
-            p { (time_left.get()) }
+    create_effect(cloned!((value, error_count) => move || {
+        let value = (*value.get()).clone();
+
+        if value.len() > 0 {
+            let errors = process_text(value);
+            error_count.set(errors);
+        }
+    }));
+
+    sycamore::render(|| {
+        template! {
+            div(class="wrapper") {
+                p { (time_left.get()) }
+                p { (error_count.get()) }
+                div(id="quote") {
+                    Keyed(KeyedProps {
+                        iterable: current_quote.handle(),
+                        template: |c| template! {
+                            span { (c) }
+                        },
+                        key: |c| *c
+                    })
+                }
+                input(bind:value=value)
+            }
         }
     });
 }
---
+++ typers/utils.js
@@ -0,0 +1,35 @@
+function processText(value) {
+    const value_array = value.split('');
+    let errors = 0;
+
+    let quoteSpanArray = document.getElementById("quote").querySelectorAll('span');
+    quoteSpanArray.forEach((char, index) => {
+        let typedChar = value_array[index];
+
+        switch (typedChar) {
+            case undefined:
+                char.classList.remove('correct');
+                char.classList.remove('incorrect');
+                break;
+            case char.innerText:
+                char.classList.add('correct');
+                char.classList.remove('incorrect');
+                break;
+            default:
+                char.classList.remove('correct');
+                char.classList.add('incorrect');
+                errors++;
+                break;
+        }
+    });
+
+    return errors;
+}
+
+function reset() {
+    let quoteSpanArray = document.getElementById("quote").querySelectorAll('span');
+    quoteSpanArray.forEach((char, _) => {
+        char.classList.remove('correct');
+        char.classList.remove('incorrect');
+    })
+}
