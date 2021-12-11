4bac221923f8f3f11da0a357256d7ec7a44d59f8
Yash Karandikar <nerdstep710@gmail.com>
2021-12-10 20:51:42 -0600

    Add ability to get id from button

--- riddlrs/src/main.rs
+++ riddlrs/src/main.rs
@@ -1,13 +1,17 @@
-use sycamore::prelude::*;
+#![allow(unused_macros)]
+
 use indexmap::IndexMap;
+use std::panic;
+use sycamore::prelude::*;
 use wasm_bindgen::prelude::*;
+use web_sys::Event;
 
 macro_rules! indexmap {
     ($($key:expr => $value:expr),*) => {{
         let mut map = IndexMap::with_capacity(5);
 
         $(
-            map.insert($key, $value);            
+            map.insert($key, $value);
         )*
 
         map
@@ -45,11 +49,32 @@ macro_rules! wasm_import {
     }
 }
 
-#[derive(Clone, Debug)]
+macro_rules! wasm_import_type {
+    ($name:ident) => {
+        #[wasm_bindgen]
+        extern "C" {
+            pub type $name;
+        }
+    };
+    ($($name:ident),*) => {
+        #[wasm_bindgen]
+        extern "C" {
+            $(pub type $name;)*
+        }
+    }
+}
+
+macro_rules! read_js_value {
+    ($target:expr, $key:expr) => {
+        js_sys::Reflect::get(&$target, &wasm_bindgen::JsValue::from_str($key))
+    };
+}
+
+#[derive(Clone, Debug, Default)]
 struct Question {
     prompt: &'static str,
     answer: char,
-    choices: IndexMap<char, &'static str>
+    choices: IndexMap<char, &'static str>,
 }
 
 macro_rules! create_question {
@@ -57,53 +82,102 @@ macro_rules! create_question {
         Question {
             prompt: $prompt,
             answer: $answer,
-            choices: $choices
+            choices: $choices,
         }
-    }
+    };
 }
 
 wasm_import!(log(s: String));
 
 fn main() {
-    let questions = [create_question!("Which of these fruits contains potassium?", 'B',
-        indexmap! {
-            'A' => "apple",
-            'B' => "banana",
-            'C' => "cherry",
-            'D' => "dragonfruit"
-        }
-    ),
-    create_question!("The moon's light actually comes from the sun", 'A', indexmap! {
-        'A' => "True",
-        'B' => "False"
-    }),
-    create_question!("Which of these countries is not considered a kingdom?", 'C', indexmap! {
-        'A' => "Belgium",
-        'B' => "Denmark",
-        'C' => "Monaco",
-        'D' => "Sweden"
-    }),
-    create_question!("Force is measured in which unit?", 'B', indexmap! {
-        'A' => "Kilograms",
-        'B' => "Newtons",
-        'C' => "Joules"
-    }),
-    create_question!("What does a vulcanologist study?", 'A', indexmap! {
-        'A' => "Volcanoes",
-        'B' => "Plants",
-        'C' => "Constellations"
-    }),
-    create_question!("Energy is measured in which unit?", 'C', indexmap! {
-        'A' => "Kilograms",
-        'B' => "Newtons",
-        'C' => "Joules"
-    }),
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let questions = [
+        create_question!(
+            "Which of these fruits contains potassium?",
+            'B',
+            indexmap! {
+                'A' => "apple",
+                'B' => "banana",
+                'C' => "cherry",
+                'D' => "dragonfruit"
+            }
+        ),
+        create_question!(
+            "The moon's light actually comes from the sun",
+            'A',
+            indexmap! {
+                'A' => "True",
+                'B' => "False"
+            }
+        ),
+        create_question!(
+            "Which of these countries is not considered a kingdom?",
+            'C',
+            indexmap! {
+                'A' => "Belgium",
+                'B' => "Denmark",
+                'C' => "Monaco",
+                'D' => "Sweden"
+            }
+        ),
+        create_question!(
+            "Force is measured in which unit?",
+            'B',
+            indexmap! {
+                'A' => "Kilograms",
+                'B' => "Newtons",
+                'C' => "Joules"
+            }
+        ),
+        create_question!(
+            "What does a vulcanologist study?",
+            'A',
+            indexmap! {
+                'A' => "Volcanoes",
+                'B' => "Plants",
+                'C' => "Constellations"
+            }
+        ),
+        create_question!(
+            "Energy is measured in which unit?",
+            'C',
+            indexmap! {
+                'A' => "Kilograms",
+                'B' => "Newtons",
+                'C' => "Joules"
+            }
+        ),
     ];
-    log(format!("{:#?}", questions));
-    sycamore::render(|| template! {
+    let index = Signal::new(0usize);
+    let current_prompt = create_memo(cloned!((index, questions) => move || {
+        questions[*index.get()].prompt.to_string()
+    }));
+    let current_choices = create_memo(cloned!((index, questions) => move || {
+        questions[*index.get()].choices.clone().into_iter().collect::<Vec<(char, &str)>>()
+    }));
+
+    let answer_question = cloned!((index, questions) => move |e: Event| {
+        let answer = questions[*index.get()].answer;
+        let id = read_js_value!(e.target().unwrap(), "id").unwrap().as_string().unwrap().chars().collect::<Vec<char>>()[0];
+        log(format!("{}", id == answer));
+    });
+
+    sycamore::render(cloned!(answer_question => || view! {
         div(class="wrapper") {
             h1(class="text-align-center") { "RiddlRS" }
-            p { "Hello, world!" }
+            div(class="card") {
+                p(class="text-align-center") { (current_prompt.get()) }
+                div(class="text-align-center") {
+                    Keyed(KeyedProps {
+                        iterable: current_choices,
+                        template: move |(c, s)| view! {
+                            button(class="fw", id=c, on:click=answer_question.clone()) { (c)") "(s) }
+                        },
+                        key: |(c, _)| *c
+                    })
+                }
+            }
         }
-    });
+    }));
 }
