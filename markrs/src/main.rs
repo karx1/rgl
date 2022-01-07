3e6af92383250be13c5431f04ef9b1c01d38e145
Yash Karandikar <nerdstep710@gmail.com>
2022-01-07 15:18:28 -0600

    Make layout

--- markrs/src/main.rs
+++ markrs/src/main.rs
@@ -1,10 +1,19 @@
 use sycamore::prelude::*;
 
 fn main() {
+    let value = Signal::new(String::new());
+
     sycamore::render(|| view! {
         h1(class="text-align-center") { "MarkRS" }
-        div(class="wrapper") {
-            p { "Hello, world!" }
+        div(class="flex-container-row wrapper") {
+            div(class="flex-child") {
+                div(class="flex-container-column") {
+                    textarea(bind:value=value, class="flex-child")
+                    div(class="card flex-child") {
+                        "TODO"
+                    }
+                }
+            }
         }
         div(class="footer") {
             "Made with Rust 1.57.0 and WASM"
