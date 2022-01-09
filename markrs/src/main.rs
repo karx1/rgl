e3cb443542e2d4c7c0536f3a127564972da34856
Yash Karandikar <nerdstep710@gmail.com>
2022-01-08 18:48:05 -0600

    Rustfmt

--- markrs/src/main.rs
+++ markrs/src/main.rs
@@ -4,18 +4,20 @@ fn main() {
     let value = Signal::new(String::new());
     let rendered = create_memo(cloned!(value => move || markdown::to_html(&*value.get())));
 
-    sycamore::render(|| view! {
-        h1(class="text-align-center") { "MarkRS" }
-        div(class="flex-container-row wrapper") {
-            div(class="flex-child") {
-                div(class="flex-container-column") {
-                    textarea(bind:value=value, class="flex-child")
-                    div(class="card flex-child", dangerously_set_inner_html=&*rendered.get())
+    sycamore::render(|| {
+        view! {
+            h1(class="text-align-center") { "MarkRS" }
+            div(class="flex-container-row wrapper") {
+                div(class="flex-child") {
+                    div(class="flex-container-column") {
+                        textarea(bind:value=value, class="flex-child")
+                        div(class="card flex-child", dangerously_set_inner_html=&*rendered.get())
+                    }
                 }
             }
-        }
-        div(class="footer") {
-            "Made with Rust 1.57.0 and WASM"
+            div(class="footer") {
+                "Made with Rust 1.57.0 and WASM"
+            }
         }
     });
 }
