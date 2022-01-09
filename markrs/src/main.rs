2fb32f7b2857ea271ceed13f8e981335bbe3a7fa
Yash Karandikar <nerdstep710@gmail.com>
2022-01-08 19:21:42 -0600

    Re-render every second

--- markrs/src/main.rs
+++ markrs/src/main.rs
@@ -1,8 +1,56 @@
 use sycamore::prelude::*;
+use wasm_bindgen::prelude::*;
+
+macro_rules! wasm_import {
+    ($name:ident()) => {
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen]
+            pub fn $name();
+        }
+    };
+    ($name:ident( $( $arg:ident: $type:ty ),* )) => {
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen]
+            pub fn $name($($arg: $type),*);
+        }
+    };
+    ($name:ident($($arg:ident: $type:ty),*) > $ret:ty) => {
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen]
+            pub fn $name($($arg: $type),*) -> $ret;
+        }
+    };
+    ($name:ident() > $ret:ty) => {
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen]
+            pub fn $name() -> $ret;
+        }
+    }
+}
+
+wasm_import!(setInterval(closure: &Closure<dyn FnMut()>, ms: u32) > f64);
+wasm_import!(clearInterval(id: f64));
 
 fn main() {
     let value = Signal::new(String::new());
-    let rendered = create_memo(cloned!(value => move || markdown::to_html(&*value.get())));
+    // let rendered = create_memo(cloned!(value => move || markdown::to_html(&*value.get())));
+    let rendered = Signal::new(String::new());
+
+    let cb = cloned!((value, rendered) => Closure::wrap(Box::new(move || {
+        rendered.set(markdown::to_html(&*value.get()));
+    }) as Box<dyn FnMut()>));
+
+    let id = setInterval(&cb, 1000);
+
+    cb.forget();
+
+    on_cleanup(move || {
+        clearInterval(id);
+    });
 
     sycamore::render(|| {
         view! {
