361d9107c0917b5041badccdbcfd04c60190b0d5
Yash Karandikar <nerdstep710@gmail.com>
2022-01-16 22:39:25 -0600

    Add ability to decrypt

--- cryptrs/src/main.rs
+++ cryptrs/src/main.rs
@@ -15,7 +15,6 @@ enum AppMode {
 
 #[component(EncryptionComponent<G>)]
 fn encryption_component() -> View<G> {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
     let keyword = Signal::new(String::new());
     let input = Signal::new(String::new());
 
@@ -53,10 +52,10 @@ fn encryption_component() -> View<G> {
 
     view! {
         label { "Keyword:" 
-            input(placeholder="key", bind:value=keyword)
+            input(bind:value=keyword)
         }
         label { "Input:"
-            input(placeholder="secrets", bind:value=input)
+            input(bind:value=input)
         }
         div(class="card") {
             (crypted.get())
@@ -64,16 +63,69 @@ fn encryption_component() -> View<G> {
     }
 }
 
+#[component(DecryptionComponent<G>)]
+fn decryption_component() -> View<G> {
+    let keyword = Signal::new(String::new());
+    let input = Signal::new(String::new());
+    
+    let decrypted = create_memo(cloned!((keyword, input) => move || {
+        let keyword = &**keyword.get();
+
+        let mut key = String::new();
+
+        for c in keyword.chars().cycle() {
+            if key.len() == input.get().len() {
+                break;
+            } else {
+                key.push(c.to_ascii_uppercase());
+            }
+        }
+
+        log(&format!("{}", key));
+
+        let mut generated = String::with_capacity(input.get().len());
+
+        for (i, c) in input.get().chars().enumerate() {
+            let char = key.chars().nth(i).unwrap(); // we can safely assume that this is not None because we made the key the same length as input
+
+            let mut x = (c.to_ascii_uppercase() as i16 - char as i16 + 26) as u16 % 26;
+
+            x += 'A' as u16;
+
+            generated.push(char::from_u32(x as u32).unwrap());
+        }
+
+        generated
+    }));
+
+    view! {
+        label { "Keyword:"
+            input(bind:value=keyword)
+        }
+        label { "Input:"
+            input(bind:value=input)
+        }
+        div(class="card") {
+            (decrypted.get())
+        }
+    }
+}
+
 fn main() {
-    let mode = Signal::new(AppMode::Encrypt);
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let mode = Signal::new(AppMode::Decrypt);
 
     sycamore::render(|| {
         view! {
             h1(class="text-align-center") { "CryptRS" }
+            div(class="text-align-center") {
+                button { "Encrypt" }
+                button { "Decrypt" }
+            }
             div(class="wrapper") {
                 (match *mode.get() {
                     AppMode::Encrypt => view! { EncryptionComponent() },
-                    AppMode::Decrypt => todo!()
+                    AppMode::Decrypt => view! { DecryptionComponent() }
                 })
             }
         }
