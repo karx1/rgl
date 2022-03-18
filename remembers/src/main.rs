4d93e7148fe6e80a290f857a304c6e69085cea33
Yash Karandikar <nerdstep710@gmail.com>
2022-03-18 11:43:33 -0500

    Initial commit

---
+++ remembers/src/main.rs
@@ -0,0 +1,9 @@
+use sycamore::prelude::*;
+
+fn main() {
+    sycamore::render(|ctx| view! {ctx, 
+        div(class="wrapper") {
+            h1(class="text-align-center") { "RemembeRS" }
+        }
+    });
+}
