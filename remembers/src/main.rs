6a8a1ef10724dc10ccc52562802ce062591916ff
Yash Karandikar <nerdstep710@gmail.com>
2022-03-20 18:31:30 -0500

    dyn_ref to the rescue

--- remembers/src/main.rs
+++ remembers/src/main.rs
@@ -1,5 +1,6 @@
 use sycamore::prelude::*;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{Event, Element};
 
 macro_rules! wasm_import {
     ($($tt:tt)*) => {
@@ -9,6 +10,9 @@ macro_rules! wasm_import {
             pub fn $($tt)*;
         }
     };
+}
+
+macro_rules! wasm_import_with_ns {
     ($ns: ident, $($tt:tt)*) => {
         #[wasm_bindgen]
         extern "C" {
@@ -16,42 +20,25 @@ macro_rules! wasm_import {
             pub fn $($tt)*;
         }
     };
-    ($name: ident, $($tt:tt)*) => {
-        #[wasm_bindgen]
-        extern "C" {
-            #[wasm_bindgen(js_name = $name)]
-            pub fn $($tt)*;
-        }
-    };
-    ($name:ident, $ns: ident, $($tt:tt)*) => {
-        #[wasm_bindgen]
-        extern "C" {
-            #[wasm_bindgen(js_name = $name, js_namespace = $ns)]
-            pub fn $($tt)*;
-        }
-    };
 }
 
 wasm_import!(toggle_cards());
+wasm_import_with_ns!(console, log(s: &str));
 
 fn main() {
     sycamore::render(|ctx| {
         console_error_panic_hook::set_once();
 
-        let first_render = ctx.create_signal(true);
-        ctx.create_effect(|| {
-            if !*first_render.get() {
-                toggle_cards();
-            }
-            first_render.set(false);
-            println!("{}", first_render.get());
-        });
+        let on_click = |event: Event| {
+            let elem = event.target().unwrap().dyn_ref::<Element>().unwrap().clone();
+            elem.class_list().toggle("flip").unwrap();
+        };
 
         view! {ctx, 
         div(class="wrapper") {
             h1(class="text-align-center") { "RemembeRS" }
             div(id="game") {
-                div(class="card") {
+                div(class="card", on:click=on_click) {
                     "Hello, world!"
                 }
             }
