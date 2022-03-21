3d877709f336315b67729893d13b915acd7ab945
Yash Karandikar <nerdstep710@gmail.com>
2022-03-21 11:41:57 -0500

    Use randomly shuffled strings instead of numbers

--- remembers/src/main.rs
+++ remembers/src/main.rs
@@ -1,3 +1,4 @@
+use rand::seq::SliceRandom;
 use sycamore::prelude::*;
 use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::{Element, Event};
@@ -29,7 +30,16 @@ fn main() {
     sycamore::render(|ctx| {
         console_error_panic_hook::set_once();
 
-        let v: &Signal<Vec<u8>> = ctx.create_signal((0..12u8).collect());
+        let mut cards = ["burger", "fries", "hotdog", "soda", "nachos", "tacos"]
+            .into_iter()
+            .cycle()
+            .take(12)
+            // .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+
+        cards.shuffle(&mut rand::rngs::OsRng);
+
+        let v = ctx.create_signal(cards);
 
         let on_click = |event: Event| {
             let elem = event
@@ -43,21 +53,21 @@ fn main() {
 
         view! {ctx,
             div(class="wrapper") {
-                h1(class="text-align-center") { "RemembeRS" }
-                section(id="game") {
-                    Keyed {
-                        iterable: v,
-                        view: move |ctx, i| view! {ctx,
-                            div(class="card", on:click=on_click) {
-                                h2(class="back-face") {
-                                    (i)
-                                }
-                                div(class="front-face")
+            h1(class="text-align-center") { "RemembeRS" }
+            section(id="game") {
+                Keyed {
+                    iterable: v,
+                    view: move |ctx, i| view! {ctx,
+                        div(class="card", on:click=on_click, data_value=i.clone()) {
+                            h2(class="back-face") {
+                                (i)
                             }
-                        },
-                        key: |x| *x
-                    }
+                            div(class="front-face")
+                        }
+                    },
+                    key: |x| x.clone()
                 }
+            }
             }
         }
     });
