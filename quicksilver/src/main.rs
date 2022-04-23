7f2b3f0d72a7860fa80e32d368ac1e3c8c41fa1b
Yash Karandikar <nerdstep710@gmail.com>
2022-04-23 11:53:36 -0500

    Now we can show and switch between cards

--- quicksilver/src/main.rs
+++ quicksilver/src/main.rs
@@ -36,24 +36,55 @@ impl Deref for Deck {
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 struct Card {
-    question: String,
-    answers: [String; 4],
-    correct: usize,
+    front: String,
+    back: String,
 }
 
-wasm_import!(get_token() -> String);
+wasm_import!(get_token() -> Option<String>);
 wasm_import_with_ns!(console, log(s: &str));
 
+#[component]
+fn CardsComponent<G: Html>(ctx: Scope) -> View<G> {
+    let token = get_token().unwrap();
+    let data = String::from_utf8(base64::decode(&token).unwrap()).unwrap();
+    let deck: Deck = serde_json::from_str(&data).unwrap();
+    let current = create_signal(ctx, rand::thread_rng().gen_range(0..deck.len()));
+    log(&format!("{:#?}", *deck));
+    let deck_len = deck.len();
+
+    let recompute_current = move |_| {
+        current.set(rand::thread_rng().gen_range(0..deck_len));
+    };
+
+    view! {ctx,
+        button(on:click=recompute_current) { "Next" }
+        ({
+            let current_card = deck[*current.get()].clone();
+            view! {ctx,
+                div {
+                    (current_card.front)
+                }
+                div {
+                    (current_card.back)
+                }
+            }
+        })
+    }
+}
+
 fn main() {
+    console_error_panic_hook::set_once();
     sycamore::render(|ctx| {
-        let token = get_token();
-        let data = String::from_utf8(base64::decode(&token).unwrap()).unwrap();
-        log(&data);
-        let deck: Deck = serde_json::from_str(&data).unwrap();
-        let current = create_signal(ctx, rand::thread_rng().gen_range(0..deck.len()));
         view! {ctx,
             div(class="wrapper") {
                 h1(class="text-align-center") { "Quicksilver" }
+                (if get_token().is_some() {
+                    view! {ctx, CardsComponent {
+
+                    }}
+                } else {
+                    view! {ctx, }
+                })
             }
         }
     });
